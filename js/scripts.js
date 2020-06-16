d8bb242ed89fcf0e92e5d44916430a5c48e53138
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-16 15:42:23 -0600

    return a value based on the calculation function.
    co-Authored-By Michael Watts <wattsjmichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,25 @@
-function BankAccount () {
+function BankAccount (initialDeposit, depositAmount, withdrawalAmount, balance) {
   this.initialDeposit = initialDeposit;
   this.depositAmount = depositAmount;
   this.withdrawalAmount = withdrawalAmount;
-  this.balance = "";
+  this.balance = balance;
 }
 
 BankAccount.prototype.bankAccountBalance = function() {
   return this.initialDeposit + this.depositAmount - this.withdrawalAmount;
 }
 
-
 $(document).ready(function() {
   $("#bank").submit(function(event) {
   event.preventDefault()
   
-  let name = $("#name").val();
-  let initialDeposit = $("#initialDeposit").val();
-  let depositAmount = $("#depositAmount").val();
-  let withdrawalAmount =  $("#withdrawalAmount").val();
-  let newBankAccount = new BankAccount(name, initialDeposit, depositAmount, withdrawalAmount)
+  // let name = $("#name").val();
+  let initialDeposit = parseInt($("#initialDeposit").val());
+  let depositAmount = parseInt($("#depositAmount").val());
+  let withdrawalAmount =  parseInt($("#withdrawalAmount").val());
+  let newBankAccount = new BankAccount(initialDeposit, depositAmount, withdrawalAmount)
   $("#output").show();
-  $("#balance").text(newBankAccount.bankAccountBalance);
-    console.log();
+  $("#balance").text(newBankAccount.bankAccountBalance());
+  $("#bank").hide();
   });
 });
